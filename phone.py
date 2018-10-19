9fb577d315795da588c0b7df785f615e7a5a995f
Natacha Quintero <natachaquintero@client78-10-192-127.eduroam.upc.edu>
2018-10-20 01:30:54 +0200

    adding some code

--- phone.py
+++ phone.py
@@ -7,7 +7,7 @@ Created on Fri Oct 19 23:40:36 2018
 """
 
 import requests
-#import cv2
+import cv2
 import numpy as np
 
 url = "http://10.4.230.168:8080/shot.jpg"
@@ -15,4 +15,9 @@ url = "http://10.4.230.168:8080/shot.jpg"
 while True:
     new_image= requests.get(url)
     img_arr= np.array(bytearray(new_image.content),dtype=np.uint8)
+    img=cv2.imdecode(img_arr,-1)
+    cv22.omshow"
+
+    
+    
     
