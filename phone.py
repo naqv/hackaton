23f31efc6ef42965201c5fe37ac4986bf3ad5b26
Natacha Quintero <natachaquintero@client78-10-192-127.eduroam.upc.edu>
2018-10-20 01:13:20 +0200

    Script

---
+++ phone.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 23:40:36 2018
+
+@author: natachaquintero
+"""
+
+import requests
+#import cv2
+import numpy as np
+
+url = "http://10.4.230.168:8080/shot.jpg"
+
+while True:
+    new_image= requests.get(url)
+    img_arr= np.array(bytearray(new_image.content),dtype=np.uint8)
+    
